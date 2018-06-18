f68c264985096f309a1f7930775ef3ec02d98268
Home-tec <Home-tec@Home-tec-kom>
2018-06-18 21:54:38 +0200

    Interfejs połaczenia z bazą danych

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Jakiś tytuł</title>
+    </head>
+    <body>
+        
+        Zaczynamy ostrą pracę nad własnym CMSem ;)
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
